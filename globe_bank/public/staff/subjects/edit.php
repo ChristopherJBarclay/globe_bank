7f9cdde080d0caabeb1155f30560176d0135c499
Christopher Barclay <christopherjbarclay@gmail.com>
2019-08-12 18:11:56 -0700

    Add files via upload
    Fixed the issue from last commit.

--- globe_bank/public/staff/subjects/edit.php
+++ globe_bank/public/staff/subjects/edit.php
@@ -38,13 +38,13 @@ if(is_post_request()) {
       <form action="<?php echo url_for('/staff/subjects/edit.php?id=' . h(u($id))); ?>" method="post">
           <dl>
             <dt>Menu Name</dt>
-            <dd><input type="text" name="menu_name" value="<?php echo h($subject['$menu_name']); ?>" /></dd>
+            <dd><input type="text" name="menu_name" value="<?php echo h($subject['menu_name']); ?>" /></dd>
           </dl>
           <dl>
             <dt>Position</dt>
             <dd>
               <select name="position">
-                <option value="1"<?php if($subject['$position'] =="1") { echo " selected"; } ?>>1</option>
+                <option value="1"<?php if($subject['position'] =="1") { echo " selected"; } ?>>1</option>
               </select>
             </dd>
           </dl>
@@ -52,7 +52,7 @@ if(is_post_request()) {
             <dt>Visible</dt>
             <dd>
               <input type="hidden" name="visible" value="0" />
-              <input type="checkbox" name="visible" value="1"<?php if($subject['$visible'] =="1") { echo " checked"; } ?> />
+              <input type="checkbox" name="visible" value="1"<?php if($subject['visible'] =="1") { echo " checked"; } ?> />
             </dd>
           </dl>
           <div id="operations">
