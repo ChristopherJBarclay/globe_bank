eba149990c48c503ccec5ab0377162c27bf4e259
Christopher Barclay <christopherjbarclay@gmail.com>
2019-09-23 12:20:49 -0700

    final files

--- globe_bank/public/staff/index.php
+++ globe_bank/public/staff/index.php
@@ -1,7 +1,6 @@
-<?php // initialize.php still needs the ../../ file path dots,
-    // because this is where the other file paths are defined ?>
-
 <?php require_once('../../private/initialize.php'); ?>
+
+<?php require_login(); ?>
 
 <?php $page_title = 'Staff Menu'; ?>
 <?php include(SHARED_PATH . '/staff_header.php'); ?>
@@ -11,7 +10,6 @@
     <h2>Main Menu</h2>
     <ul>
       <li><a href="<?php echo url_for('/staff/subjects/index.php'); ?>">Subjects</a></li>
-      <li><a href="<?php echo url_for('/staff/pages/index.php'); ?>">Pages</a></li>
       <li><a href="<?php echo url_for('/staff/admins/index.php'); ?>">Admins</a></li>
     </ul>
   </div>
--- globe_bank/public/staff/login.php
+++ globe_bank/public/staff/login.php
@@ -10,9 +10,34 @@ if(is_post_request()) {
   $username = isset($_POST['username']) ? $_POST['username'] : '';
   $password = isset($_POST['password']) ? $_POST['password'] : '';
 
-  $_SESSION['username'] = $username;
+  // Validations
+  if(is_blank($username)) {
+    $errors[] = "Username cannot be blank.";
+  }
+  if(is_blank($password)) {
+    $errors[] = "Password cannot be blank.";
+  }
 
-  redirect_to(url_for('/staff/index.php'));
+  // if there were no errors, try to login.
+  if(empty($errors)) {
+    // using one variable ensures that msg is the same
+    $login_failure_msg = "Log in was unsuccessful.";
+    
+    $admin = find_admin_by_username($username);
+    if ($admin) {
+      if (password_verify($password, $admin['hashed_password'])) {
+        // true = password matches
+        log_in_admin($admin);
+        redirect_to(url_for('/staff/index.php'));
+      } else {
+        // false = username found, but password does not match
+        $errors[] = $login_failure_msg;
+      }
+    } else {
+      // no username found
+      $errors[] = $login_failure_msg;
+    }
+  }
 }
 
 ?>
